29038087066690d64ef6fe3ac8e3476bbce3abc3
fanyuzeng <393568551@qq.com>
2017-10-30 18:28:30 +0800

    HasMoreData使用

--- app/src/main/java/com/project/fanyuzeng/mvpdemo/model/SohuAlbumModel.java
+++ app/src/main/java/com/project/fanyuzeng/mvpdemo/model/SohuAlbumModel.java
@@ -24,19 +24,24 @@ public class SohuAlbumModel implements IBaseModel {
 
     @Override
     public void sendRequestToServer() {
-        OkHttpManager.getInstance().requestServerData(method, url, mPaginationPresenter.getParams(), new DisposeDataHandler(new DisposeDataListener() {
-            @Override
-            public void onSuccess(Object responseObj) {
-                String responseJson = (String) responseObj;
-                Log.d(TAG, ">> onSuccess >> " + responseJson);
-                mPaginationPresenter.accessSuccess(responseJson);
-            }
-            @Override
-            public void onFailure(OkHttpException exception) {
-                Log.d(TAG, ">> onFailure >> " + exception.getErrorCode());
-                mPaginationPresenter.okHttpError(exception.getErrorCode(), exception.getErrorMsg(), url);
-            }
-        },null));
+        if (mPaginationPresenter.hasMoreData()) {
+            OkHttpManager.getInstance().requestServerData(method, url, mPaginationPresenter.getParams(), new DisposeDataHandler(new DisposeDataListener() {
+                @Override
+                public void onSuccess(Object responseObj) {
+                    String responseJson = (String) responseObj;
+                    Log.d(TAG, ">> onSuccess >> " + responseJson);
+                    mPaginationPresenter.accessSuccess(responseJson);
+                }
+
+                @Override
+                public void onFailure(OkHttpException exception) {
+                    Log.d(TAG, ">> onFailure >> " + exception.getErrorCode());
+                    mPaginationPresenter.okHttpError(exception.getErrorCode(), exception.getErrorMsg(), url);
+                }
+            }, null));
+        }else {
+            Log.d(TAG,">> sendRequestToServer >> " + "No more data!");
+        }
     }
 
     @Override
